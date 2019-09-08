8370e75b2297f3daec6fe3188033f1d6b69a0a82
Patrick Palczewski <psykiatris@gmail.com>
2019-09-08 08:40:02 -0700

    Fixed integer inputs from Integer.parseInt() to in.nextInt().

--- src/org/palczewski/Main.java
+++ src/org/palczewski/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double balance;
+
         try (Scanner in = new Scanner(System.in, StandardCharsets.UTF_8)) {
 
             System.out.println("Nifty interest calculator");
             System.out.printf("See the interest you could earn in one month.%n");
             System.out.print("Enter month:  ");
-            int month = Integer.parseInt(in.nextLine());
+            int month = in.nextInt();
             System.out.print("Enter year: ");
-            int year = Integer.parseInt(in.nextLine());
+            int year = in.nextInt();
             System.out.print("Enter balance: ");
-            balance = in.nextDouble();
+            double balance = in.nextDouble();
             System.out.print("Enter interest rate: ");
             double intRate = in.nextDouble();
 
