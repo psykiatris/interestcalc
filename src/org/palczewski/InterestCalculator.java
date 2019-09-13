fbfe8470601f47bbd0f467d20839cf51a900132d
Patrick Palczewski <psykiatris@gmail.com>
2019-09-13 16:35:58 -0700

    Added InterestCalculator class to combine other classes, for simplicity.

---
+++ src/org/palczewski/InterestCalculator.java
@@ -0,0 +1,10 @@
+package org.palczewski;
+/*
+Consolicate ConsoleLoop and CompundInterest classes into one for
+simplicity.
+
+This will be the main function to be called from Main, passing the
+parameters. Will add other methods as needed
+ */
+public class InterestCalculator {
+}
