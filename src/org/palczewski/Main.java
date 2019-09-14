deed785603396ef36674f0e8e40fbfcc464d9420
Patrick Palczewski <psykiatris@gmail.com>
2019-09-14 06:40:02 -0700

    Converted if statements to a switch block.

--- src/org/palczewski/Main.java
+++ src/org/palczewski/Main.java
@@ -32,7 +32,7 @@ public class Main {
             System.out.printf("\tEnter an option:%n\t%2d - %-11s%n\t%2d - %-11s%n\t%2d - %-11s%n", 1, "Savings", 2, "Credit Card", 9, "Quit");
 
             int choice = 0;
-            // Params o pass to function
+            // Params to pass to function
             LocalDate startDate;
             int cycle = 0;
             double apr = 0;
@@ -41,54 +41,57 @@ public class Main {
             do {
                 System.out.print("Make a choice: ");
                 choice = in.nextInt();
-                if(choice == 1) {
-                    System.out.print("You chose Savings\r");
+                switch (choice) {
+                    case 1: {
+                        System.out.print("You chose Savings\r");
+                        System.out.println();
+                        // Get more info from user
+                        System.out.print("Enter start of month: ");
+                        startDate = LocalDate.parse(in.next());
+                        int year = startDate.getYear();
+                        int month = startDate.getMonthValue();
+                        YearMonth ym = YearMonth.of(year, month);
+                        cycle = ym.lengthOfMonth();
+                        LocalDate endDate = startDate.plus(cycle - 1,
+                                ChronoUnit.DAYS);
+                        System.out.printf("Statement period: %1$tm-%1$td-%1$tY to %2$tm-%2$td-%2$tY%n",
+                                startDate, endDate);
+                        System.out.printf("Cycle is %d days.%n", cycle);
+                        System.out.print("Now enter the APR: ");
+                        apr = in.nextDouble();
+                        System.out.printf("Using an APR of %2.2f%%%n", apr);
+                        // Now the info is collected, pass to function
 
-                    System.out.println();
-                    // Get more info from user
-                    System.out.print("Enter start of month: ");
-                    startDate = LocalDate.parse(in.next());
-                    int year = startDate.getYear();
-                    int month = startDate.getMonthValue();
-                    YearMonth ym = YearMonth.of(year, month);
-                    cycle = ym.lengthOfMonth();
-                    LocalDate endDate = startDate.plus(cycle -1,
-                            ChronoUnit.DAYS);
-                    System.out.printf("Statement period: %1$tm-%1$td-%1$tY to %2$tm-%2$td-%2$tY%n",
-                            startDate, endDate);
-                    System.out.printf("Cycle is %d days.%n", cycle);
-                    System.out.print("Now enter the APR: ");
-                    apr = in.nextDouble();
-                    System.out.printf("Using an APR of %2.2f%%%n", apr);
-                    // Now the info is collected, pass to function
 
+                        flag = false;
 
-
-                    flag = false;
-
-                } else if (choice == 2) {
-                    System.out.println("You chose Credit Card");
-                    System.out.println();
-                    // Get more info from user
-                    System.out.print("Enter statement start date: ");
-                    startDate = LocalDate.parse(in.next());
-                    cycle = 30;
-                    LocalDate endDate = startDate.plus(cycle -1,
-                            ChronoUnit.DAYS);
-                    System.out.printf("Cycle period: %1$tm-%1$td-%1$tY " +
-                                    "to " +
-                                    "%2$tm-%2$td-%2$tY%n",
-                            startDate, endDate);
-                    System.out.printf("Cycle: %d days.%n", cycle);
-                    System.out.print("Enter the APR: ");
-                    apr = in.nextDouble();
-                    System.out.printf("Using APR of %2.2f%%%n", apr);
-                    flag = false;
-                } else if (choice == 9) {
-                    System.out.println("Exiting chice menu.");
-                    System.exit(0);
-                } else {
-                    System.out.println("You did not make a valid choice");
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("You chose Credit Card");
+                        System.out.println();
+                        // Get more info from user
+                        System.out.print("Enter statement start date: ");
+                        startDate = LocalDate.parse(in.next());
+                        cycle = 30;
+                        LocalDate endDate = startDate.plus(cycle - 1,
+                                ChronoUnit.DAYS);
+                        System.out.printf("Cycle period: %1$tm-%1$td-%1$tY " +
+                                        "to %2$tm-%2$td-%2$tY%n",
+                                startDate, endDate);
+                        System.out.printf("Cycle: %d days.%n", cycle);
+                        System.out.print("Enter the APR: ");
+                        apr = in.nextDouble();
+                        System.out.printf("Using APR of %2.2f%%%n", apr);
+                        flag = false;
+                        break;
+                    }
+                    case 9:
+                        System.out.println("Exiting chice menu.");
+                        System.exit(0);
+                    default:
+                        System.out.println("You did not make a valid choice");
+                        break;
                 }
             } while (flag);
 
