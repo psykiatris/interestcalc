2d01c2b00cfcee8953a794d1a2b9982877b644d5
Patrick Palczewski <psykiatris@gmail.com>
2019-09-14 05:18:36 -0700

    Finished choic menu. Corrected some typos.

--- src/org/palczewski/Main.java
+++ src/org/palczewski/Main.java
@@ -27,7 +27,7 @@ public class Main {
          */
         try (Scanner in = new Scanner(System.in, StandardCharsets.UTF_8)) {
 
-            System.out.println("\tBoyd interest calculator");
+            System.out.println("\tBoyd Interest Calculator");
             System.out.printf("Calculates interest earned or charged%non a bank savings or credit card account.%n");
             System.out.printf("\tEnter an option:%n\t%2d - %-11s%n\t%2d - %-11s%n\t%2d - %-11s%n", 1, "Savings", 2, "Credit Card", 9, "Quit");
 
@@ -35,7 +35,6 @@ public class Main {
             // Params o pass to function
             LocalDate startDate;
             int cycle = 0;
-            double balance = 0;
             double apr = 0;
             boolean flag = true;
             // Process the choice
@@ -47,7 +46,7 @@ public class Main {
 
                     System.out.println();
                     // Get more info from user
-                    System.out.print("Enter beginning date: ");
+                    System.out.print("Enter start of month: ");
                     startDate = LocalDate.parse(in.next());
                     int year = startDate.getYear();
                     int month = startDate.getMonthValue();
@@ -60,6 +59,7 @@ public class Main {
                     System.out.printf("Cycle is %d days.%n", cycle);
                     System.out.print("Now enter the APR: ");
                     apr = in.nextDouble();
+                    System.out.printf("Using an APR of %2.2f%%%n", apr);
                     // Now the info is collected, pass to function
 
 
@@ -68,9 +68,24 @@ public class Main {
 
                 } else if (choice == 2) {
                     System.out.println("You chose Credit Card");
+                    System.out.println();
+                    // Get more info from user
+                    System.out.print("Enter statement start date: ");
+                    startDate = LocalDate.parse(in.next());
+                    cycle = 30;
+                    LocalDate endDate = startDate.plus(cycle -1,
+                            ChronoUnit.DAYS);
+                    System.out.printf("Cycle period: %1$tm-%1$td-%1$tY " +
+                                    "to " +
+                                    "%2$tm-%2$td-%2$tY%n",
+                            startDate, endDate);
+                    System.out.printf("Cycle: %d days.%n", cycle);
+                    System.out.print("Enter the APR: ");
+                    apr = in.nextDouble();
+                    System.out.printf("Using APR of %2.2f%%%n", apr);
                     flag = false;
                 } else if (choice == 9) {
-                    System.out.println("Exiting");
+                    System.out.println("Exiting chice menu.");
                     System.exit(0);
                 } else {
                     System.out.println("You did not make a valid choice");
@@ -79,7 +94,7 @@ public class Main {
 
             // Finsihing up.
             System.out.println();
-            System.out.println("Thank you for using Boyc Calculator!");
+            System.out.println("Thank you for using Boyd Calculator!");
 
         }
 
