f6d8b6fc1a883e4b9e58e94c7ae979124b4d393a
Patrick Palczewski <psykiatris@gmail.com>
2019-11-04 07:01:58 -0800

    Finished BaseAccount class with toString and getters. Also, set up a self-test main.

--- src/org/palczewski/BaseAccount.java
+++ src/org/palczewski/BaseAccount.java
@@ -1,5 +1,8 @@
 package org.palczewski;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDate;
 
 /*
@@ -10,30 +13,50 @@ public class BaseAccount {
     private final double balance;
     private final double apr;
     private static double period = 0.0;
+    private final LocalDate date;
     
-    BaseAccount(double balance, double apr, LocalDate date) {
+    BaseAccount(double balance, double apr, @NotNull LocalDate date) {
         this.balance = balance;
         this.apr = apr;
-        LocalDate d = date;
-        period = d.lengthOfMonth();
+        this.date = date;
+        period = date.lengthOfMonth();
 
     }
 
+    @Contract(pure = true)
     public final double getBalance() {
         return balance;
     }
 
+    @Contract(pure = true)
     public final double getApr() {
         return apr;
     }
 
+    @Contract(pure = true)
     public final double getPeriod() {
         return period;
+    }
+
+    @Contract(pure = true)
+    public final LocalDate getDate() {
+        return date;
     }
 
 
     @Override
     public final String toString() {
-        return String.format("Balance: %s%nAPR: %s%n", balance, apr);
+        return String.format("Balance: $%,.2f%nAPR: %.2f%%%nDate: %3$tB " +
+                        "%3$tY",
+                balance,
+                apr, date);
+    }
+
+    // Self-test
+    public static void main(String[] args) {
+        String m = "2020-02-15";
+        BaseAccount acct = new BaseAccount(1000, 1.88, LocalDate.parse(m));
+        System.out.println(acct);
+        System.out.printf("Days in period: %.0f%n", period);
     }
 }
