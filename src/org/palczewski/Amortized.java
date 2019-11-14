8a880e3ddc89ee657178d242e945d8c0fb28a907
Patrick Palczewski <psykiatris@gmail.com>
2019-11-14 05:20:34 -0800

    Refining the formula for amortized payments.

--- src/org/palczewski/Amortized.java
+++ src/org/palczewski/Amortized.java
@@ -12,10 +12,10 @@ public class Amortized {
 
 
     Amortized(double p, double rate, double periods) {
-        double moInt = (rate / 100) / 12;
+        double moInt = rate / (12 * 100);
 
         double payment =
-                p * (moInt / (1 - (Math.pow(1/(1 + moInt), periods))));
+                p * (moInt / (1 - (StrictMath.pow(1/(1 + moInt), periods))));
         System.out.printf("Based on principal of $%,.2f for %.0f months," +
                         "%nat %.2f%%, your payments would be $%,.2f%n", p
                 , periods,
