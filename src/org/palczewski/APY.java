6b241f1d78eb1a1535ee66677113c41f005c13cb
Patrick Palczewski <psykiatris@gmail.com>
2019-11-15 07:29:01 -0800

    Created growth table for APY.

--- src/org/palczewski/APY.java
+++ src/org/palczewski/APY.java
@@ -1,5 +1,6 @@
 package org.palczewski;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class APY {
@@ -11,12 +12,31 @@ public class APY {
         double form = 1 + (apr / compoundRate);
 
         // The rest of the formula
-        double res = (Math.pow(form, compoundRate) - 1);
+        double res = (StrictMath.pow(form, compoundRate) - 1);
 
         System.out.printf("Balance: $%,.2f%nInterest rate: %.2f%%%nInterest paid yearly: $%,.2f%n= monthly: $%,.2f%n", balance,
                 intRate,
                 balance * res, (balance * res) / 12);
         System.out.printf("True APY is: %,.2f%%%n", res * 100);
+
+    }
+
+    private static void growthTable(double startBal, double rate, LocalDate date
+            , double invBal, double term) {
+
+        LocalDate localDate = date;
+        double bal = startBal;
+        double moRate = rate / (12 * 100);
+        double month = term * 12;
+
+        for(int i = 0; i < month; i++) {
+            double moInt = bal * moRate;
+            double newBal = bal + moInt;
+            System.out.printf("%1$tb %tY - Bal: $%,.2f Int: $%,.2f%n",
+                    localDate, newBal, moInt);
+            bal = newBal + invBal;
+            localDate = localDate.plusMonths(1);
+        }
     }
 
     public static void main(String[] args) {
@@ -26,9 +46,15 @@ public class APY {
             double bal = in.nextDouble();
             System.out.print("Enter interest rate: ");
             double interest = in.nextDouble();
+            System.out.print("Enter amount to invest monthly: ");
+            double invBal = in.nextDouble();
+            System.out.print("Enter term in years: ");
+            double term = in.nextDouble();
+
+            growthTable(bal, interest,LocalDate.now(), invBal, term);
 
             // Pass along
-            new APY(bal, interest);
+            // new APY(bal, interest);
         }
 
     }
