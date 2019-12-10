8d5b3a5e6bee0d28e302968103eb9de2c3a8d294
Patrick Palczewski <psykiatris@gmail.com>
2019-12-10 07:03:28 -0800

    Created proposed package to write experimental code without breaking main process. Working on streams.

---
+++ src/org/palczewski/proposed/StreamGenerator.java
@@ -0,0 +1,23 @@
+package org.palczewski.proposed;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamGenerator {
+
+    public Stream<List<LocalDate>> generateMonth(LocalDate date) {
+        List<LocalDate> daysList = new ArrayList<>(1);
+        int cycle = date.lengthOfMonth();
+        LocalDate endDate = date.plusDays(cycle);
+
+        return Stream.of(daysList);
+
+    }
+
+    // Self-test method
+    public static void main(String[] args) {
+
+    }
+}
