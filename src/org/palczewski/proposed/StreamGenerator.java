d1ec3f26812ebba8c70a9584aae62aeefa6f2e08
Patrick Palczewski <psykiatris@gmail.com>
2019-12-14 06:26:43 -0800

    Successfully created stream of LocalDate

--- src/org/palczewski/proposed/StreamGenerator.java
+++ src/org/palczewski/proposed/StreamGenerator.java
@@ -7,10 +7,19 @@ import java.util.stream.Stream;
 
 public class StreamGenerator {
 
-    public Stream<List<LocalDate>> generateCycleDays(LocalDate date) {
+    /**
+     * Creates a stream of dates in a cycle
+     * @param date The beginning date
+     * @return A List of dates
+     */
+    public static Stream<List<LocalDate>> generateCycleDays(LocalDate date) {
         List<LocalDate> daysList = new ArrayList<>(1);
         int cycle = date.lengthOfMonth();
         LocalDate endDate = date.plusDays(cycle);
+        for(int i = 0; i < cycle; i++) {
+            daysList.add(date);
+            date = date.plusDays(1);
+        }
 
         return Stream.of(daysList);
 
@@ -18,6 +27,17 @@ public class StreamGenerator {
 
     // Self-test method
     public static void main(String[] args) {
+        // Create a date
+        LocalDate date = LocalDate.now();
+        // Print results of returned stream
+        Object[] res = generateCycleDays(date).toArray();
+
+        for (Object o : res) {
+            System.out.println(o);
+        }
+
+
+
 
     }
 }
