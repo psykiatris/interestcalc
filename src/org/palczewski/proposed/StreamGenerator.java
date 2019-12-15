68abe79b45db07d20401629a7e1adc44a196a441
Patrick Palczewski <psykiatris@gmail.com>
2019-12-15 05:31:29 -0800

    Working on streams

--- src/org/palczewski/proposed/StreamGenerator.java
+++ src/org/palczewski/proposed/StreamGenerator.java
@@ -3,6 +3,7 @@ package org.palczewski.proposed;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Spliterator;
 import java.util.stream.Stream;
 
 public class StreamGenerator {
@@ -12,13 +13,14 @@ public class StreamGenerator {
      * @param date The beginning date
      * @return A List of dates
      */
-    public static Stream<List<LocalDate>> generateCycleDays(LocalDate date) {
+    private static Stream<List<LocalDate>> generateCycleDays(LocalDate date) {
+        LocalDate local = date;
         List<LocalDate> daysList = new ArrayList<>(1);
-        int cycle = date.lengthOfMonth();
-        LocalDate endDate = date.plusDays(cycle);
+        int cycle = local.lengthOfMonth();
+        LocalDate endDate = local.plusDays(cycle);
         for(int i = 0; i < cycle; i++) {
-            daysList.add(date);
-            date = date.plusDays(1);
+            daysList.add(local);
+            local = local.plusDays(1);
         }
 
         return Stream.of(daysList);
@@ -30,11 +32,28 @@ public class StreamGenerator {
         // Create a date
         LocalDate date = LocalDate.now();
         // Print results of returned stream
-        Object[] res = generateCycleDays(date).toArray();
+        List<Spliterator<List<LocalDate>>> res = new ArrayList<>(1);
+        Stream<List<LocalDate>> resStream = generateCycleDays(date);
 
-        for (Object o : res) {
-            System.out.println(o);
+        /*
+        for(LocalDate d = date; d.equals(date.isBefore(date.plusDays(date.lengthOfMonth()))); d = date.plusDays(1)) {
+
+
         }
+
+         */
+
+
+
+
+
+
+
+
+        // Length
+        System.out.println(String.format("Cycle is: %d days",
+                res.size()));
+
 
 
 
