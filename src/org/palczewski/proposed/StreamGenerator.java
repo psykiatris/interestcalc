d9a8397048a656dfe1add332244d797d8229288d
Patrick Palczewski <psykiatris@gmail.com>
2019-12-15 07:09:06 -0800

    Working on streams

--- src/org/palczewski/proposed/StreamGenerator.java
+++ src/org/palczewski/proposed/StreamGenerator.java
@@ -2,8 +2,11 @@ package org.palczewski.proposed;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Spliterator;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamGenerator {
@@ -13,17 +16,16 @@ public class StreamGenerator {
      * @param date The beginning date
      * @return A List of dates
      */
-    private static Stream<List<LocalDate>> generateCycleDays(LocalDate date) {
+    private static Stream<LocalDate> generateCycleDays(LocalDate date) {
         LocalDate local = date;
         List<LocalDate> daysList = new ArrayList<>(1);
         int cycle = local.lengthOfMonth();
-        LocalDate endDate = local.plusDays(cycle);
         for(int i = 0; i < cycle; i++) {
             daysList.add(local);
             local = local.plusDays(1);
         }
 
-        return Stream.of(daysList);
+        return daysList.stream();
 
     }
 
@@ -32,22 +34,14 @@ public class StreamGenerator {
         // Create a date
         LocalDate date = LocalDate.now();
         // Print results of returned stream
-        List<Spliterator<List<LocalDate>>> res = new ArrayList<>(1);
-        Stream<List<LocalDate>> resStream = generateCycleDays(date);
 
-        /*
-        for(LocalDate d = date; d.equals(date.isBefore(date.plusDays(date.lengthOfMonth()))); d = date.plusDays(1)) {
-
-
-        }
-
-         */
+        List<LocalDate> res =
+                generateCycleDays(date)
+                        .collect(Collectors.toList());
 
 
-
-
-
-
+        System.out.println(res);
+        System.out.printf("First day of cycle: %s%n", res.get(0));
 
 
         // Length
