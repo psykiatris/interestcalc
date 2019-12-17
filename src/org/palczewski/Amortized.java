96d6589ee6c299053bee9cfac8f4ce9c9cb039c1
Patrick Palczewski <psykiatris@gmail.com>
2019-12-17 06:14:48 -0800

    Documented Amortized class and changed to private.

--- src/org/palczewski/Amortized.java
+++ src/org/palczewski/Amortized.java
@@ -2,34 +2,48 @@ package org.palczewski;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
-/*
+
+/**
 Caclulates amoritzed payments for loans, mortgages, etc.
+ @author patrick palczewski
  */
 public class Amortized {
 
-    Amortized(double p, double rate, double periods) {
+    /**
+     *
+     * @param p Principal balance
+     * @param rate Annual Percentage Rate
+     * @param periods Months in period
+     *
+     */
+    private Amortized(double p, double rate, double periods) {
+
         double moInt = rate / (12 * 100);
 
+        // Formula to calculate payment
         double payment =
                 p * (moInt / (1 - (StrictMath.pow(1/(1 + moInt), periods))));
         // print table
         amortizationSchedule(p, rate, LocalDate.now(),
 
                 periods / 12, payment);
-        System.out.println("\t===Summary of payments===");
-        System.out.printf("Based on principal of $%,.2f for %.0f months," +
-                        "%nat %.3f%%, your payments would be $%,.2f%n", p
-                , periods,
-                rate,
-                payment);
+        System.out.println("\t=== Payment Summary ===");
+        System.out.printf("Based on principal of $%,.2f for %.0f months,%nat %.3f%%, your payments would be $%,.2f%n",
+                p, periods, rate, payment);
 
 
     }
 
+    /**
+     *
+     * @param prin principal balance
+     * @param rate interest rate
+     * @param startDate start date of period
+     * @param years term in years
+     * @param pmt calculated p & i payment
+     */
     private void amortizationSchedule(double prin, double rate,
                                       LocalDate startDate, double years,
                                       double pmt) {
@@ -39,7 +53,7 @@ public class Amortized {
         double moRate = rate / (12 * 100);
         double period = years * 12;
 
-        System.out.println("\t===Amortization Table===");
+        System.out.println("\t=== Amortization Table ===");
         // Loop to display table
         for(int i = 0; i < period; i++) {
             double moInterest = v * moRate;
