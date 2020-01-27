845c93bcb35c9da2309e2f031f3f42fe1bd5d4a2
Patrick Palczewski <psykiatris@gmail.com>
2020-01-27 06:31:54 -0800

    Mucking with DoPeriod()

--- src/org/palczewski/DoPeriod.java
+++ src/org/palczewski/DoPeriod.java
@@ -12,51 +12,81 @@ import java.util.Scanner;
  * @version .99
  */
 public class DoPeriod {
+    // TODO: 1/27/20 Rewrite this class for simplicity and clarity.
+    /**
+     * Empty constructor
+     */
+    private DoPeriod() {
+
+    }
 
     /**
-     * Calculates days between period of dates
+     * Displays period in days
+     * @param start Start date
+     * @param cycle Number of days
      */
-    DoPeriod() {
+    private void daysInPeriod(LocalDate start, int cycle) {
+
+        LocalDate endDate = start.plus(cycle,
+                ChronoUnit.DAYS);
+        System.out.printf("Period is from %1$tm-%1$td-%1$tY to %2$tm-%2$td-%2$tY%n",
+                start, endDate);
+        System.out.printf("Period is %s days.%n", cycle);
+
+
+
+    }
+
+    /**
+     * Displays period in weeks
+     * @param start Start date
+     * @param cycle Number of days
+     */
+    private void weeksInPeriod(LocalDate start, int cycle) {
+        LocalDate endDate = start.plus(cycle,
+                ChronoUnit.DAYS);
+        System.out.printf("Period is from %1$tm-%1$td-%1$tY to %2$tm-%2$td-%2$tY%n",
+                start, endDate);
+        System.out.printf("Period is %s weeks.%n", cycle / 7);
+
+    }
+
+    /**
+     * Collects info from user
+     */
+    public final void collect() {
+        LocalDate start;
+        LocalDate end;
         try(Scanner in = new Scanner(System.in, StandardCharsets.UTF_8)) {
             LocalDate startDate;
             System.out.print("Enter begining date: ");
             startDate = LocalDate.parse(in.next());
             System.out.print("Enter days in period: ");
             int cycle = in.nextInt();
-            LocalDate endDate = startDate.plus(cycle,
-                    ChronoUnit.DAYS);
-            System.out.printf("Period is from %1$tm-%1$td-%1$tY to %2$tm-%2$td-%2$tY%n",
-                    startDate, endDate);
-            System.out.printf("Period is %s days.%n",
-                    daysInPeriod(startDate, endDate));
+
+            start = startDate;
+            daysInPeriod(start, cycle);
+            weeksInPeriod(start, cycle);
+
         }
-    }
 
-    /**
-     * Calculates days in period
-     * @param start Start date
-     * @param end End date
-     * @return Number of days
-     */
-    private int daysInPeriod(LocalDate start, LocalDate end) {
-
-        return Period.between(start, end).getDays();
-
-    }
-
-    /**
-     * Returns number of weeks between dates
-     * @param start Start date
-     * @param end End date
-     * @return Number of weeks
-     */
-    private int weeksInPeriod(LocalDate start, LocalDate end) {
-        // empty for now
-        return 0;
     }
 
     // Self-test
     public static void main(String[] args) {
-        new DoPeriod();
+        try(Scanner in = new Scanner(System.in, StandardCharsets.UTF_8)) {
+            System.out.print("Enter date: ");
+            LocalDate start = LocalDate.parse(in.next());
+            System.out.print("Number of weeks: ");
+            int weeks = in.nextInt();
+
+            LocalDate end = start.plusWeeks(weeks);
+            System.out.printf("Dates from: %s to %s,%n for a total of %s" +
+                            " weeks.%n",
+                    start,
+                    end,
+                    weeks);
+
+        }
     }
 }
