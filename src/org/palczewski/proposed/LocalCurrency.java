2cfa39b06475179e75c98a49f13be178a3484f66
Patrick Palczewski <psykiatris@gmail.com>
2021-03-13 15:53:52 -0800

    Provided more document to the LocalCurrency class and added another currency.

--- src/org/palczewski/proposed/LocalCurrency.java
+++ src/org/palczewski/proposed/LocalCurrency.java
@@ -14,13 +14,15 @@ public class LocalCurrency {
 
     public static void main(String[] args) {
 
-        
-        String usDollar = Currency.getInstance(Locale.getDefault()).getSymbol();
+        // Gets currency from system
+        String sysCurrency = Currency.getInstance(Locale.getDefault()).getSymbol();
 
+        // Hard coded currency
         String pound = Currency.getInstance(Locale.UK).getSymbol();
+        String yen = Currency.getInstance(Locale.JAPAN).getSymbol();
 
-        System.out.printf("US Dollar: %s\nBritish Pound: %s", usDollar,
-                pound);
+        System.out.printf("US Dollar: %s\nBritish Pound: %s\nJapanese Yen: %s", sysCurrency,
+                pound, yen);
 
     }
 }
